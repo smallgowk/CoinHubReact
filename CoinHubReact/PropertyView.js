50bc49ca2856800e85c61ad871a1389009d6703d
Phan Nang Duy <phanduy.itbka@gmail.com>
2018-09-21 00:23:15 +0700

    no message

--- CoinHubReact/PropertyView.js
+++ CoinHubReact/PropertyView.js
@@ -27,21 +27,79 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
+ 
 'use strict';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react'
 import {
-  createStackNavigator,
-} from 'react-navigation';
-import SearchPage from './SearchPage';
-import SearchResults from './SearchResults';
-import PropertyView from './PropertyView';
+  StyleSheet,
+  Image,
+  View,
+  Text
+} from 'react-native';
 
-const App = createStackNavigator({
-  Home: { screen: SearchPage },
-  Results: { screen: SearchResults },
-  Property: { screen: PropertyView},
+type Props = {};
+export default class PropertyView extends Component<Props> {
+  static navigationOptions = {
+    title: 'Property',
+  };
+
+  render() {
+    const { params } = this.props.navigation.state;
+    var property = params.property;
+    var stats = property.bedroom_number + ' bed ' + property.property_type;
+    if (property.bathroom_number) {
+      stats += ', ' + property.bathroom_number + ' ' + (property.bathroom_number > 1
+        ? 'bathrooms' : 'bathroom');
+    }
+
+    var price = property.price_formatted.split(' ')[0];
+
+    return (
+      <View style={styles.container}>
+        <Image style={styles.image}
+            source={{uri: property.img_url}} />
+        <View style={styles.heading}>
+          <Text style={styles.price}>{price}</Text>
+          <Text style={styles.title}>{property.title}</Text>
+          <View style={styles.separator}/>
+        </View>
+        <Text style={styles.description}>{stats}</Text>
+        <Text style={styles.description}>{property.summary}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 65
+  },
+  heading: {
+    backgroundColor: '#F8F8F8',
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#DDDDDD'
+  },
+  image: {
+    width: 400,
+    height: 300
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    margin: 5,
+    color: '#48BBEC'
+  },
+  title: {
+    fontSize: 20,
+    margin: 5,
+    color: '#656565'
+  },
+  description: {
+    fontSize: 18,
+    margin: 5,
+    color: '#656565'
+  }
 });
-export default App;
